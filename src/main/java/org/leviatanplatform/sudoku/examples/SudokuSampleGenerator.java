52d8d46c2567c4b0a083646ef904c1b899c3ab35
CiudadAlex <ciudad.alex@gmail.com>
2023-10-02 01:13:54 +0200

    Sudoku

---
+++ src/main/java/org/leviatanplatform/sudoku/examples/SudokuSampleGenerator.java
@@ -0,0 +1,18 @@
+package org.leviatanplatform.sudoku.examples;
+
+public class SudokuSampleGenerator {
+
+    public static final Integer[][] EASY_1 = new Integer[][] {
+            { null,    8, null, null, null,    1,    6, null, null },
+            { null,    7, null,    4, null, null, null,    2,    1 },
+            {    5, null, null,    3,    9,    6, null, null, null },
+
+            {    2, null,    4, null,    5, null,    1, null, null },
+            { null, null,    8,    9, null,    7,    5, null, null },
+            { null,    5,    7, null,    3, null,    9, null, null },
+
+            { null, null, null,    5,    6,    3, null, null,    9 },
+            {    3,    1, null, null, null,    2, null,    5, null },
+            { null, null,    5,    8, null, null, null,    4, null },
+    };
+}
