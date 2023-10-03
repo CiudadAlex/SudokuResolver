98bd87374cddd56bb973a54df8d8b29cef315765
CiudadAlex <ciudad.alex@gmail.com>
2023-10-03 22:07:24 +0200

    Sudoku

--- src/main/java/org/leviatanplatform/sudoku/engine/HypothesizerSudokuResolver.java
+++ src/main/java/org/leviatanplatform/sudoku/engine/HypothesizerSudokuResolver.java
@@ -1,21 +1,48 @@
 package org.leviatanplatform.sudoku.engine;
 
+import org.leviatanplatform.sudoku.engine.validation.BoardValidationException;
+import org.leviatanplatform.sudoku.engine.validation.BoardValidator;
+
 import java.util.Set;
 
 public class HypothesizerSudokuResolver {
 
     public static Board solve(Board board) {
 
-        Board modBoard = SudokuResolver.solve(board);
+        BoardValidator.validateBoard(board);
 
-        if (!modBoard.isFinished()) {
-            Position positionWithLessCandidates = getPositionWithLessCandidates(board);
-            // FIXME select candidate and set it in copied board and iterate until finished or exception. If exception try an other candidate
+        if (board.isFinished()) {
+            return board;
         }
 
-        // FIXME finish
+        Board modBoard = SudokuResolver.solve(board);
 
-        return modBoard;
+        if (modBoard.isFinished()) {
+            return modBoard;
+        }
+
+        Position positionWithLessCandidates = getPositionWithLessCandidates(modBoard);
+        int column = positionWithLessCandidates.getColumn();
+        int row = positionWithLessCandidates.getRow();
+
+        for (Integer itemCandidate : positionWithLessCandidates.getCandidates()) {
+
+            Board hypothesisBoard = modBoard.copy();
+            hypothesisBoard.set(column, row, itemCandidate);
+
+            try {
+                hypothesisBoard = solve(hypothesisBoard);
+
+                if (hypothesisBoard.isFinished()) {
+                    return hypothesisBoard;
+                }
+
+            } catch(BoardValidationException e) {
+                // Hypothesis failed
+            }
+        }
+
+        throw new BoardValidationException("Run out of possible candidates");
     }
 
     private static Position getPositionWithLessCandidates(Board board) {
